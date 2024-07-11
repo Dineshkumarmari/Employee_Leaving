26edb4aa9024a62ce1f8cd4818ec1c9b7d735dd9
Dinesh M <dhineshkumarm1999@gmail.com>
2024-07-11 12:27:07 +0530

    feat: disable extension

--- Employee_Leaving/Models/Extensions.cs
+++ Employee_Leaving/Models/Extensions.cs
@@ -5,24 +5,24 @@ namespace Employee_Leaving.Models
 {
     public static class Extensions
     {
-        public static string GetClaimRole(this IIdentity identity)
-        {
-            ClaimsIdentity claimsIdentity = identity as ClaimsIdentity;
-            Claim claim = claimsIdentity.FindFirst(ClaimTypes.Role);
-            return claim.Value;
-        }
-        public static string GetClaimName(this IIdentity identity)
-        {
-            ClaimsIdentity claimsIdentity = identity as ClaimsIdentity;
-            Claim Name = claimsIdentity.FindFirst(ClaimTypes.Name);
-            return Name.Value;
+        //public static string GetClaimRole(this IIdentity identity)
+        //{
+        //    ClaimsIdentity claimsIdentity = identity as ClaimsIdentity;
+        //    Claim claim = claimsIdentity.FindFirst(ClaimTypes.Role);
+        //    return claim.Value;
+        //}
+        //public static string GetClaimName(this IIdentity identity)
+        //{
+        //    ClaimsIdentity claimsIdentity = identity as ClaimsIdentity;
+        //    Claim Name = claimsIdentity.FindFirst(ClaimTypes.Name);
+        //    return Name.Value;
 
-        }
-        public static string GetClaimValue(this IIdentity identity)
-        {
-            ClaimsIdentity claimsIdentity = identity as ClaimsIdentity;
-            Claim Id = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
-            return Id.Value;
-        }
+        //}
+        //public static string GetClaimValue(this IIdentity identity)
+        //{
+        //    ClaimsIdentity claimsIdentity = identity as ClaimsIdentity;
+        //    Claim Id = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+        //    return Id.Value;
+        //}
     }
 }
