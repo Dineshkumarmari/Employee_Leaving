167ef00abb04123533ef4fe4e35318e04641deee
Prathap M <arunachalam.subramani@amtote.com>
2024-07-11 11:07:09 +0530

    feat: add nullable employee

--- Employee_Leaving/Models/EmployeeLeaveDetails.cs
+++ Employee_Leaving/Models/EmployeeLeaveDetails.cs
@@ -7,12 +7,12 @@ namespace Employee_Leaving.Models
         public int LeaveId { get; set; }
         public int LeaveTypeId { get; set; }
         public int Emp_Id { get; set; }
-        public string EmployeeName { get; set; }
-        public string Leavename { get; set; }
+        public string? EmployeeName { get; set; }
+        public string? Leavename { get; set; }
         public DateTime StartingDate { get; set; }
         public DateTime EndingDate { get; set; }
         public int TotalNoDays { get; set; }
-        public string ActionResult { get; set; }
+        public string? ActionResult { get; set; }
         public int RemainingLeaveDays { get; set; }
     }
 }
